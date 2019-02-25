41e3085d4afa50aed39647d8b6e45fa28d18b789
rahulambastha23 <47660381+rahulambastha23@users.noreply.github.com>
2019-02-25 14:23:23 +0530

    Add files via upload

--- display_tt.py
+++ display_tt.py
@@ -2,10 +2,9 @@ from tkinter import *
 from tkinter import ttk
 
 root = Tk()
-root.geometry("500x200")
+root.geometry("1000x800")
 
 timeTable_4A = [
-    ['Day', 'Lecture 1', 'Lecture 2', 'Lecture 3', 'Lecture 4', '', 'Lecture 5', 'Lecture 6', 'Lecture 7'],
     ['MON', 'DS', 'DS', 'CSA', 'OOPS', 'B', 'OS', 'CM', 'DMS'],
     ['TUE', 'CSA', 'OOPS', 'H/W LAB', 'H/W LAB', 'R', 'OS', 'DMS', 'CM'],
     ['WED', 'OOPS', 'OS', 'OOPS LAB', 'OOPS LAB', 'E', 'DS', 'DMS', 'CSA'],
@@ -17,23 +16,29 @@ timeTable_4A = [
 frame = Frame(root)
 frame.pack()
 
-tree = ttk.Treeview(frame, columns = (1,2,3,4,5,6), height = 10, show = "headings")
+tree = ttk.Treeview(frame, columns = (1,2,3,4,5,6,7,8,9), height = 5, show = "headings")
 tree.pack(side = 'left')
 
-tree.heading(1, text="Column 1")
-tree.heading(2, text="Column 2")
-tree.heading(3, text="Column 3")
-tree.heading(4, text="Column 4")
-tree.heading(5, text="Column 5")
-tree.heading(6, text="Column 6")
+tree.heading(1, text="Day")
+tree.heading(2, text="Lecture 1")
+tree.heading(3, text="Lecture 2")
+tree.heading(4, text="Lecture 3")
+tree.heading(5, text="Lecture 4")
+tree.heading(6, text="")
+tree.heading(7, text="Lecture 5")
+tree.heading(8, text="Lecture 6")
+tree.heading(9, text="Lecture 7")
 
-tree.column(1, width = 100)
-tree.column(2, width = 100)
-tree.column(3, width = 100)
-tree.column(4, width = 100)
-tree.column(5, width = 100)
-tree.column(6, width = 100)
+tree.column(1, width = 100, anchor = "center")
+tree.column(2, width = 100, anchor = "center")
+tree.column(3, width = 100, anchor = "center")
+tree.column(4, width = 100, anchor = "center")
+tree.column(5, width = 100, anchor = "center")
+tree.column(6, width = 100, anchor = "center")
+tree.column(7, width = 100, anchor = "center")
+tree.column(8, width = 100, anchor = "center")
+tree.column(9, width = 100, anchor = "center")
 for val in timeTable_4A:
-    tree.insert('', 'end', values = (val[0], val[1], val[2],val[3],val[4],val[5],val[6]) )
+    tree.insert('', 'end', values = (val[0], val[1], val[2],val[3],val[4],val[5],val[6], val[7], val[8]) )
 
 root.mainloop()
